35675b9c6995c307e7c538d0c18a4cb021b313bb
Marcio <dev@marciof.com>
2017-06-27 18:32:58 -0700

    Remove unnecessary Memory Region iterator class

--- interpreter/i386/multiboot/Info.cpp
+++ interpreter/i386/multiboot/Info.cpp
@@ -10,18 +10,6 @@
 extern "C" multiboot_info* e_multiboot_info;
 extern "C" uint32_t e_multiboot_magic_nr;
 
-class Memory_Region_Iterator {
-public:
-    Memory_Region_Iterator(multiboot_mmap_entry* array, size_t size_bytes);
-    bool has_next();
-    multiboot_mmap_entry* next();
-
-private:
-    multiboot_mmap_entry* _array;
-    size_t _position;
-    size_t _size_bytes;
-};
-
 E_BIT_ATTR_PACKED(struct Boot_Device {
     // Partition numbers start at zero.
     enum {BOOT_DEVICE_UNUSED_PARTITION = 0xFF};
@@ -31,24 +19,6 @@ E_BIT_ATTR_PACKED(struct Boot_Device {
     uint8_t top_level_partition;
     e_Multiboot_Drive_BIOS_Nr drive_number;
 });
-
-Memory_Region_Iterator::Memory_Region_Iterator(
-    multiboot_mmap_entry* array, size_t size_bytes) :
-    _array(array), _position(0), _size_bytes(size_bytes) {
-}
-
-bool Memory_Region_Iterator::has_next() {
-    return this->_position < this->_size_bytes;
-}
-
-multiboot_mmap_entry* Memory_Region_Iterator::next() {
-    uint8_t* address = reinterpret_cast<uint8_t*>(_array) + _position;
-    multiboot_mmap_entry* region
-        = reinterpret_cast<multiboot_mmap_entry*>(address);
-
-    _position += sizeof(region->size) + region->size;
-    return region;
-}
 
 static void log_boot_device(struct multiboot_info* info, struct e_Log* log) {
     if (IS_FLAG_SET(info->flags, MULTIBOOT_INFO_BOOTDEV)) {
@@ -98,15 +68,18 @@ static void log_memory_map(struct multiboot_info* info, struct e_Log* log) {
         return;
     }
 
-    Memory_Region_Iterator iterator(
-        reinterpret_cast<multiboot_mmap_entry*>(info->mmap_addr),
-        info->mmap_length);
+    multiboot_mmap_entry* array = reinterpret_cast<multiboot_mmap_entry*>(info->mmap_addr);
+    size_t position = 0;
+    size_t size_bytes = info->mmap_length;
 
     // The memory map is provided by the BIOS and is guaranteed to list
     // all standard memory that should be available for normal use.
 
-    while (iterator.has_next()) {
-        multiboot_mmap_entry* region = iterator.next();
+    while (position < size_bytes) {
+        uint8_t* address = reinterpret_cast<uint8_t*>(array) + position;
+        multiboot_mmap_entry* region = reinterpret_cast<multiboot_mmap_entry*>(address);
+
+        position += sizeof(region->size) + region->size;
 
         e_Log_msg(log, E_LOG_INFO, "Memory map region: addr=[{iuh}, {iuh}]; "
             "size=[{iuh}, {iuh}] B; type={iu}",
