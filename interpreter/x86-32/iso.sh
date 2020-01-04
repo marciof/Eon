aead002415ad2c853a5471cc49501542bbc65ce8
Marcio <dev@marciof.com>
2020-01-03 20:09:59 -0800

    Fix `mktemp` pattern to not include any path separators

--- interpreter/x86-32/iso.sh
+++ interpreter/x86-32/iso.sh
@@ -7,27 +7,28 @@ if [ $# -ne 1 ]; then
 fi
 
 kernel_file="$1"
-cd_image="$(mktemp -t "$kernel_file-grub-cd-XXXXX.iso")"
-cd_contents="$(mktemp -d)"
+kernel_file_name="$(basename "$kernel_file")"
+cd_image_file="$(mktemp -t "$kernel_file_name-grub-cd-XXXXX.iso")"
+cd_contents_dir="$(mktemp -d)"
 
-cp "$kernel_file" "$cd_contents"
-mkdir -p "$cd_contents/boot/grub"
+cp "$kernel_file" "$cd_contents_dir/"
+mkdir -p "$cd_contents_dir/boot/grub"
 
-cat <<CONFIG >"$cd_contents/boot/grub/grub.cfg"
+cat <<CONFIG >"$cd_contents_dir/boot/grub/grub.cfg"
 set timeout=0
-menuentry "$(basename "$kernel_file")" {
+menuentry "$kernel_file_name" {
     insmod multiboot
-    multiboot /$kernel_file
+    multiboot /$kernel_file_name
 }
 CONFIG
 
-echo "Creating CD ISO from: $cd_contents" >&2
-grub-mkrescue -o "$cd_image" "$cd_contents"
-rm -rf "$cd_contents"
+echo "Creating CD ISO from: $cd_contents_dir" >&2
+grub-mkrescue -o "$cd_image_file" "$cd_contents_dir"
+rm -rf "$cd_contents_dir"
 
-if [ ! -s "$cd_image" ]; then
-    echo "Failed to create a GRUB boot CD ISO: $cd_image" >&2
+if [ ! -s "$cd_image_file" ]; then
+    echo "Failed to create a GRUB boot CD ISO: $cd_image_file" >&2
     exit 1
 fi
 
-echo "$cd_image"
+echo "$cd_image_file"
