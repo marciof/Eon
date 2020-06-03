e2b11710e46b6f5f9690b19dbe0dccef5b434da5
Marcio <dev@marciof.com>
2020-06-02 18:41:11 -0700

    Don't use non-inference filename macro in Makefile and disable echo on `rem`

--- windows-vcvarsall.bat
+++ windows-vcvarsall.bat
@@ -1,8 +1,8 @@
-rem Batch file to invoke cmd.exe and avoid quoting issues, as well as to
-rem source and setup the build environment (eg. for Travis CI).
+@rem Batch file to invoke cmd.exe and avoid quoting issues, as well as to
+@rem source and setup the build environment (eg. for Travis CI).
 
-rem Use an environment variable to choose the architecture since `shift`
-rem doesn't modify `%*`.
+@rem Use an environment variable to choose the architecture since `shift`
+@rem doesn't modify `%*`.
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" %VCVARSALL_ARCH%
 
 %*
