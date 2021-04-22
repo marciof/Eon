bc7d1253202c107347dc9720cc8aeb500fd7c968
Marcio <dev@marciof.com>
2021-04-22 21:52:31 +0100

    Add TODO on avoiding multiple evaluations on macro expansion

--- interpreter/core/Ref.h
+++ interpreter/core/Ref.h
@@ -1,10 +1,12 @@
 #pragma once
 #include <stddef.h>
 
+// TODO: error prone to multiple evaluations, use inline-able function instead?
 #define K_REF_INC(object) \
     (++(object)->ref.count, \
     (object))
 
+// TODO: ditto
 #define K_REF_DEC(object) \
     ((void) (((object) != NULL) && (--(object)->ref.count == 0) \
         ? ((object)->ref.free((object)), NULL) \
@@ -13,6 +15,7 @@
 #define K_REF_FIELD \
     struct k_Ref ref
 
+// TODO: ditto
 #define K_REF_INIT(object, free_fn) \
     ((object)->ref.count = 0, \
     (object)->ref.free = (free_fn), \
